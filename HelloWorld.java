8171c8da4370cf299d1eda31d85702d814f3d89e
saurabh sahare <saharesaurabh76@gmail.com>
2023-09-26 11:46:50 +0530

    new commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,8 @@ public class HelloWorld
     public static void main(String args[])
     {
       System.out.println("Hello,World!");
+      int a = 2,b = 4;
+      int c = a + b;
+      System.out.println(c);
     }
 }
